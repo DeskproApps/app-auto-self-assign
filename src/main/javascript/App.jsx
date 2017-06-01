4a77fb945aabd47c9c4f857bdcca4871a905e38c
radu <radu.helstern@deskpro.com>
2017-06-01 18:22:12 +0100

    assign a ticket to current user if unassigned

--- src/main/javascript/App.jsx
+++ src/main/javascript/App.jsx
@@ -5,6 +5,20 @@ export default class App extends React.Component
 {
   static propTypes = { dpapp: React.PropTypes.object.isRequired };
 
+  componentDidMount() {
+    const { dpapp } = this.props;
+
+    Promise
+      .all([ dpapp.context.getMe(), dpapp.context.getTabData()])
+      .then(([me, tabData]) => {
+        if (!tabData.agent || !tabData.agent.id) {
+          return dpapp.restApi.put(`tickets/${dpapp.context.entityId}`, { agent: me.id });
+        }
+      })
+    ;
+
+  }
+
   shouldComponentUpdate() { return false; }
 
   render() { return null; }
